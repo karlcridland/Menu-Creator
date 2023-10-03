fb1d1b063d1ee004e1bfbf7645bc886eae7a50da
Karl Cridland <karlcridland@gmail.com>
2023-10-03 08:50:44 +0100

    Search bonus results populated and normalised to be used when retrieving ingredients.

--- public/script/search-bonus.js
+++ public/script/search-bonus.js
@@ -5,16 +5,36 @@ export class SearchBonus{
     constructor(){
         this.results = {};
         this.getResults();
+        this.max = 0;
     }
 
     getResults(){
         const self = this;
         readDB('search/ingredients', (users) => {
             Object.values(users).forEach((results) => {
-                Object.entries(results).forEach((key, value) => {
-                    self.results[key] = value;
+                Object.entries(results).forEach(([key, ings]) => {
+                    self.results[key] = {};
+                    Object.entries(ings).forEach(([ing, value]) => {
+                        self.results[key][ing] = value;
+                        if (value > self.max) self.max = value;
+                    });
                 });
             });
+            self.normalise();
         });
     }
+
+    normalise(){
+        const self = this;
+        let temp = {};
+        Object.entries(self.results).forEach(([key, ings]) => {
+            let result = {};
+            Object.entries(ings).forEach(([ing, value]) => {
+                result[ing] = value / self.max;
+            })
+            temp[key] = result;
+        })
+        self.results = temp;
+    }
+
 }
